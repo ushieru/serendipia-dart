ccafa98c1ce7e6eb80136250c24871af1fb35397
ushieru <uzielcocolan@gmail.com>
2022-04-15 15:36:48 -0500

    add jwt checker

--- bin/serendipia.dart
+++ bin/serendipia.dart
@@ -2,6 +2,9 @@ import 'dart:convert';
 
 import 'package:args/args.dart';
 import 'package:serendipia/gateway.dart';
+import 'package:serendipia/helpers/config.dart';
+import 'package:serendipia/helpers/init_msg.dart';
+import 'package:serendipia/jwt_checker.dart';
 import 'package:serendipia/service_registry.dart';
 import 'package:serendipia/services_api.dart';
 import 'package:shelf/shelf.dart';
@@ -11,6 +14,15 @@ import 'package:shelf_router/shelf_router.dart';
 void main(List<String> arguments) async {
   final parser = _getParser();
   final result = parser.parse(arguments);
+  final config = Config(
+    port: int.tryParse(result['port'] as String),
+    failureThreshold: int.tryParse(result['failureThreshold'] as String),
+    cooldownPeriod: int.tryParse(result['cooldownPeriod'] as String),
+    requestTimeout: int.tryParse(result['requestTimeout'] as String),
+    heartBeat: int.tryParse(result['heartBeat'] as String),
+    jwt: result['jwt'] as String,
+  );
+
   final app = Router();
   final serviceRegistry = ServiceRegistry();
 
@@ -23,25 +35,31 @@ void main(List<String> arguments) async {
         services[service.name] = [service.toJson()];
       }
     }
-    return Response.ok(json.encode(services),
+    final payload = {'config': config.toJson(), 'services': services};
+    return Response.ok(json.encode(payload),
         headers: {'Content-Type': 'application/json'});
   });
 
   app.mount('/services', servicesApi());
 
-  app.all('/<ignored|.*>', gateway);
+  app.all('/<ignored|.*>', (Request request) async {
+    if (config.jwt.isNotEmpty && jwtChecker(request)) {
+      return Response.forbidden('You are not allowed to access this resource.');
+    }
+    final response = await gateway(request);
+    return Response(response.statusCode,
+        body: response.readAsString(), headers: response.headers);
+  });
 
   io
-      .serve(app, '0.0.0.0', int.tryParse(result['port'] as String) ?? 5000)
-      .then((server) => print(_initMsg(server.address.host, server.port)));
+      .serve(app, '0.0.0.0', config.port)
+      .then((server) => print(initMsg(server.address.host, server.port)));
 }
 
-String _initMsg(String host, int port) => ''
-    '.------------.\n'
-    '| serendipia |\n'
-    "'------------'\n"
-    'Running at '
-    'http://$host:$port';
-
-ArgParser _getParser() =>
-    ArgParser()..addOption('port', abbr: 'p', defaultsTo: '5000');
+ArgParser _getParser() => ArgParser()
+  ..addOption('port', abbr: 'p', defaultsTo: '5000')
+  ..addOption('heartBeat', abbr: 'h', defaultsTo: '5')
+  ..addOption('jwt', abbr: 'j', defaultsTo: '')
+  ..addOption('failureThreshold', abbr: 'f', defaultsTo: '5')
+  ..addOption('cooldownPeriod', abbr: 'c', defaultsTo: '10')
+  ..addOption('requestTimeout', abbr: 'r', defaultsTo: '2');
---
+++ lib/jwt_checker.dart
@@ -0,0 +1,16 @@
+import 'package:serendipia/helpers/config.dart';
+import 'package:shelf/shelf.dart';
+import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
+
+bool jwtChecker(Request request) {
+  final authHeader = request.headers['authorization'];
+  if (authHeader == null) return false;
+  final config = Config();
+  try {
+    final token = authHeader.split(' ')[1];
+    JWT.verify(token, SecretKey(config.jwt));
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
